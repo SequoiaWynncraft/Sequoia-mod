b7ab9d7984f1794b6d0dcc152954bae97ff8589b
Baptiste Giraud <42639810+ReYZhiA@users.noreply.github.com>
2026-06-29 13:10:38 +0200

    fix(events): forgor to edit that test case

--- src/test/java/com/seqwawa/seq/managers/SeqBadgeNametagRenderSupportTest.java
+++ src/test/java/com/seqwawa/seq/managers/SeqBadgeNametagRenderSupportTest.java
@@ -9,7 +9,7 @@ class SeqBadgeNametagRenderSupportTest {
     @Test
     void keepsWynntilsAndLowerBadgeOffsetsDistinct() {
         assertEquals(25f, SeqBadgeNametagRenderSupport.WYNNTILS_DEFAULT_BADGE_Y_OFFSET);
-        assertEquals(15f, SeqBadgeNametagRenderSupport.LOWER_BADGE_Y_OFFSET);
+        assertEquals(10f, SeqBadgeNametagRenderSupport.LOWER_BADGE_Y_OFFSET);
         assertNotEquals(
                 SeqBadgeNametagRenderSupport.WYNNTILS_DEFAULT_BADGE_Y_OFFSET,
                 SeqBadgeNametagRenderSupport.LOWER_BADGE_Y_OFFSET);
